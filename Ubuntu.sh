b66af7b3c17bd52af25a9b5bff7287549850124f
Mytai20100 <142713298+Mytai20100@users.noreply.github.com>
2024-11-09 14:53:50 +0700

    Ubuntu.sh

---
+++ Ubuntu.sh
@@ -0,0 +1,94 @@
+#!/bin/sh
+
+ROOTFS_DIR=$(pwd)
+export PATH=$PATH:~/.local/usr/bin
+max_retries=50
+timeout=1
+ARCH=$(uname -m)
+
+if [ "$ARCH" = "x86_64" ]; then
+  ARCH_ALT=amd64
+elif [ "$ARCH" = "aarch64" ]; then
+  ARCH_ALT=arm64
+else
+  printf "Unsupported CPU architecture: ${ARCH}"
+  exit 1
+fi
+
+if [ ! -e $ROOTFS_DIR/.installed ]; then
+  echo "#######################################################################################"
+  echo "#"
+  echo "#                                      Proot INSTALLER
+  echo "#"
+  echo "#                                    Copyright (C) 2024
+  echo "#"
+  echo "#"
+  echo "#######################################################################################"
+
+  read -p "Do you want to install Ubuntu? (YES/no): " install_ubuntu
+fi
+
+case $install_ubuntu in
+  [yY][eE][sS])
+    wget --tries=$max_retries --timeout=$timeout --no-hsts -O /tmp/rootfs.tar.gz \
+      "http://cdimage.ubuntu.com/ubuntu-base/releases/20.04/release/ubuntu-base-20.04.4-base-${ARCH_ALT}.tar.gz"
+    tar -xf /tmp/rootfs.tar.gz -C $ROOTFS_DIR
+    ;;
+  *)
+    echo "Skipping Ubuntu installation."
+    ;;
+esac
+
+if [ ! -e $ROOTFS_DIR/.installed ]; then
+  mkdir $ROOTFS_DIR/usr/local/bin -p
+  wget --tries=$max_retries --timeout=$timeout --no-hsts -O $ROOTFS_DIR/usr/local/bin/proot "https://raw.githubusercontent.com/Mytai20100/freeroot/main/proot-${ARCH}"
+
+  while [ ! -s "$ROOTFS_DIR/usr/local/bin/proot" ]; do
+    rm $ROOTFS_DIR/usr/local/bin/proot -rf
+    wget --tries=$max_retries --timeout=$timeout --no-hsts -O $ROOTFS_DIR/usr/local/bin/proot "https://raw.githubusercontent.com/Mytai20100/freeroot/main/proot-${ARCH}"
+
+    if [ -s "$ROOTFS_DIR/usr/local/bin/proot" ]; then
+      chmod 755 $ROOTFS_DIR/usr/local/bin/proot
+      break
+    fi
+
+    chmod 755 $ROOTFS_DIR/usr/local/bin/proot
+    sleep 1
+  done
+
+  chmod 755 $ROOTFS_DIR/usr/local/bin/proot
+fi
+
+if [ ! -e $ROOTFS_DIR/.installed ]; then
+  printf "nameserver 1.1.1.1\nnameserver 1.0.0.1" > ${ROOTFS_DIR}/etc/resolv.conf
+  rm -rf /tmp/rootfs.tar.xz /tmp/sbin
+  touch $ROOTFS_DIR/.installed
+fi
+
+# Get system information
+cpu_info=$(lscpu | grep "Model name:" | awk -F: '{print $2}' | xargs)
+ram_info=$(free -h | grep "Mem:" | awk '{print $2}')
+disk_info=$(df -h $ROOTFS_DIR | grep -v "Filesystem" | awk '{print $2}')
+
+CYAN='\e[0;36m'
+WHITE='\e[0;37m'
+RESET_COLOR='\e[0m'
+
+# Display system information
+echo -e "${WHITE}System Information:${RESET_COLOR}"
+echo -e "CPU Model: ${cpu_info}"
+echo -e "Total RAM: ${ram_info}"
+echo -e "Disk Size: ${disk_info}"
+
+display_gg() {
+  echo -e "${WHITE}___________________________________________________${RESET_COLOR}"
+  echo -e ""
+  echo -e "           ${CYAN}-----> Mission Completed ! <----${RESET_COLOR}"
+}
+
+clear
+display_gg
+
+$ROOTFS_DIR/usr/local/bin/proot \
+  --rootfs="${ROOTFS_DIR}" \
+  -0 -w "/root" -b /dev -b /sys -b /proc -b /etc/resolv.conf --kill-on-exit
