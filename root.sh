6447d4f33d85000f14af4e5d62476cb8c690bbfa
Mytai20100 <142713298+Mytai20100@users.noreply.github.com>
2024-11-23 13:45:49 +0700

    Update root.sh

--- root.sh
+++ root.sh
@@ -1,22 +1,10 @@
 #!/bin/sh
 
-ROOTFS_DIR=$(pwd)
-export PATH=$PATH:~/.local/usr/bin
-max_retries=50
-timeout=1
-ARCH=$(uname -m)
+options=("Ubuntu" "Debian(Unsupported)" "Kaili_linux" "CentOS")
+selected=0
 
-if [ "$ARCH" = "x86_64" ]; then
-  ARCH_ALT=amd64
-elif [ "$ARCH" = "aarch64" ]; then
-  ARCH_ALT=arm64
-else
-  printf "Unsupported CPU architecture: ${ARCH}\n"
-  exit 1
-fi
-
-# Chạy tập lệnh Ubuntu.sh theo lựa chọn của người dùng
-if [ ! -e "$ROOTFS_DIR/.installed" ]; then
+# Function to show header
+show_gg() {
   echo "#######################################################################################"
   echo "#"
   echo "#                                   RootFS INSTALLER"
@@ -24,29 +12,79 @@ if [ ! -e "$ROOTFS_DIR/.installed" ]; then
   echo "#                                  Copyright (C) 2024"
   echo "#"
   echo "#######################################################################################"
+}
 
-  echo "Please select the OS you want to install:"
-  echo "1) Ubuntu 20.04"
-  echo "2) Debian 11"
-  echo "3) Arch Linux"
-  read -p "Enter your choice (1/2/3): " os_choice
+# Hide cursor function
+hide_cursor() {
+    echo -ne "\033[?25l"
+}
 
-  case $os_choice in
-    1)
-      echo "Launching Ubuntu installation script..."
-      sh ./Ubuntu.sh
-      ;;
-    2)
-      echo "Launching Debia installation script...."
-      sh ./Debia.sh
-      ;;
-    3)
-      echo "Launching Arch installation script...."
-      sh ./Arch.sh
-      ;;
-    *)
-      echo "Invalid choice. Exiting."
-      exit 1
-      ;;
-  esac
-fi
+# Show cursor function
+show_cursor() {
+    echo -ne "\033[?25h"
+}
+
+# Print menu function
+print_menu() {
+    # Don't clear the screen here. Just print the menu and options.
+    clear # Clear the screen before printing the menu
+    show_gg # Show the header
+    echo "Use UP/DOWN arrow keys to navigate and press ENTER to select."
+    for i in "${!options[@]}"; do
+        if [ $i -eq $selected ]; then
+            echo -e "\033[1m> ${options[$i]}\033[0m" # Bold the selected option
+        else
+            echo "  ${options[$i]}"
+        fi
+    done
+}
+
+# Initial header display
+show_gg
+trap show_cursor EXIT 
+hide_cursor
+
+# Main loop for user interaction
+while true; do
+    print_menu
+    read -rsn1 key # Read one key at a time
+    case "$key" in
+        $'\x1b') # If ESC is pressed (escape sequence)
+            read -rsn2 -t 0.1 key # Read next two characters
+            case "$key" in
+                "[A") # Arrow up
+                    ((selected--))
+                    if [ $selected -lt 0 ]; then
+                        selected=$((${#options[@]} - 1)) # Wrap around to last option
+                    fi
+                    ;;
+                "[B") # Arrow down
+                    ((selected++))
+                    if [ $selected -ge ${#options[@]} ]; then
+                        selected=0 # Wrap around to the first option
+                    fi
+                    ;;
+            esac
+            ;;
+        "") # Enter key
+            show_cursor
+            echo "You selected: ${options[$selected]}"  
+            # Run the corresponding script based on the selection
+            case $selected in
+                0)
+                    bash Ubuntu.sh
+                    ;;
+                1)
+                    bash Debian.sh
+                    ;;
+                2)
+                    bash Kaili_linux.sh
+                    ;;
+                3)
+                    bash CentOS.sh
+                    ;;
+            esac
+            break
+            ;;
+    esac
+done
